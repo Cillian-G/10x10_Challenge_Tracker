7345756ce495c9080664e586457a46d3b1986559
Cillian G <cilliangoan221@gmail.com>
2022-01-24 12:12:10 +0000

    Add game-selection function

--- run.py
+++ run.py
@@ -17,5 +17,34 @@ welcome_message = "Welcome to 10x10_challenge_tracker, a handy tool for "\
                   "keeping track \nof your 10x10 challenge as you complete "\
                   "it"
 
-print(welcome_message)
+
+def welcome_menu():
+    """
+    Prints welcome menu and basic instructions
+    """
+    print(welcome_message)
+
+
+def get_game_data():
+    """
+    Takes game details input by user and records them to 
+    the google sheet for the game in question
+    """
+    game_selection_instructions = "Enter the number that corresponds to the "\
+                                  "game you wish to enter data for"
+
+    print(game_selection_instructions)
+
+    game_title = SHEET.worksheet("game_type")
+    for ind in range(1, 11):
+        title = game_title.cell(ind, 2).value
+        print(f"{ind} {title}")
+
+    game_selection = input("Enter your game selection here: ")
+    return game_selection
+
+
+welcome_menu()
+print(get_game_data())
+
 
